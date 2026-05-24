cf2db3bf461137c0efda6407fc2102572909769e
Liu Mingquan <147033006+wxqnl@users.noreply.github.com>
2026-05-24 18:41:00 +0800

    Delete fla12131231 directory

--- fla12131231/ops/forgetting_attn/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .parallel import parallel_forgetting_attn
-
-__all__ = [
-    'parallel_forgetting_attn',
-]
--- fla12131231/ops/hgrn/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .chunk import chunk_hgrn
-from .fused_recurrent import fused_recurrent_hgrn
-
-__all__ = [
-    'chunk_hgrn',
-    'fused_recurrent_hgrn',
-]
